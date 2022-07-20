03105a1bbbd5a45d6c82e6bb5646760f2d9c270d
doesnotsitproperly <doesnotsitproperly@gmail.com>
2022-07-20 06:53:58 -0400

    Update version script

--- js/version.js
+++ js/version.js
@@ -1,9 +1,11 @@
-fetch("https://api.github.com/repos/doesnotsitproperly/doesnotsitproperly.github.io/contributors")
-    .then(result => result.json())
-    .then(result => {
-        let numberOfCommits = 0;
-        result.forEach((contributor) => {
-            numberOfCommits += contributor.contributions;
+const getVersion = (repo) => {
+    fetch(`https://api.github.com/repos/doesnotsitproperly/${repo}/contributors`)
+        .then((result) => result.json())
+        .then((result) => {
+            let numberOfCommits = 0;
+            for (const contributor of result) {
+                numberOfCommits += contributor.contributions;
+            }
+            document.getElementById("version").innerHTML = `version ${numberOfCommits}`;
         });
-        document.getElementById("version").innerHTML = `version ${numberOfCommits}`;
-    });
+}
